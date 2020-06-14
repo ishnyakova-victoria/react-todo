92ea59d6676f14106f1baec2c0011283153668ac
Eugeny Bondarenko <eugeny.bondarenko@office.e2e4gu.ru>
2020-06-14 23:16:45 +0700

    - added form markup

--- src/App/Form/Form.js
+++ src/App/Form/Form.js
@@ -10,7 +10,33 @@ export class Form extends React.Component {
 
   render() {
     return (
-      <div className="form-container">This is Form Page</div>
+      <div className="form-container">
+        <form>
+          <label htmlFor="todo-title">
+            <span>Title:</span>
+            <input id="todo-title" name="title" type="text" placeholder="Enter ToDo title" />
+          </label>
+          <label htmlFor="todo-category">
+            <span>Category:</span>
+            <select id="todo-category" name="title" type="text" placeholder="Select Category">
+              <option value="">Select Category</option>
+              <option value="category-1">Category 1</option>
+              <option value="category-2">Category 2</option>
+              <option value="category-3">Category 3</option>
+              <option value="category-4">Category 4</option>
+            </select>
+          </label>
+          <label htmlFor="todo-datetime">
+            <span>Date:</span>
+            <input id="todo-title" name="title" type="datetime-local" placeholder="Pick Date & Time" />
+          </label>
+          <label htmlFor="todo-content">
+            <span>Content:</span>
+            <textarea id="todo-title" name="title" type="text" placeholder="Enter Content (optional)"></textarea>
+          </label>
+          <button>Save</button>
+        </form>
+      </div>
     );
   }
 }
