5a9d13a25299440b50e09cd38f0bca01470d9ab9
Eugeny Bondarenko <eugeny.bondarenko@office.e2e4gu.ru>
2020-07-08 23:34:21 +0700

    - added react-hook-form for form validation  - validate form via react-hook-form

--- src/App/Form/Form.js
+++ src/App/Form/Form.js
@@ -1,70 +1,57 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import './Form.css';
 import { Link, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { addTodo } from '../../Actions/TodoActions';
+import { useForm } from 'react-hook-form';
 
-export class Form extends React.Component {
-  state = {};
+export const Form = (props) => {
+  const { handleSubmit, register, setValue, errors } = useForm();
 
-  todo = {
-    title: '',
-    category: '',
-    date: '',
-    content: ''
-  };
-
-  constructor(props) {
-    super(props);
-  }
-
-  clickHandler = (event) => {
-    event.preventDefault();
-
-    this.props.addTodo({
-      id: this.props.todos.length + 1,
-      title: this.todo.title.value,
-      category: parseInt(this.todo.category.value),
-      date: this.todo.date.value,
-      content: this.todo.content.value,
+  const onSubmit = (data) => {
+    props.addTodo({
+      ...data, 
+      id: props.todos.length + 1,
+      category: parseInt(data.category)
     });
 
-    this.props.history.push('/');
+    props.history.push('/');
   };
 
-  render() {
-    return (
-      <div className="form-container">
-        <div className="manage-form-buttons">
-          <Link className="button" to="/">&lt; Back</Link>
-        </div>
-        <form method="">
-          <label htmlFor="todo-title">
-            <span>Title:</span>
-            <input id="todo-title" name="title" type="text" ref={title => this.todo.title = title} placeholder="Enter ToDo title" />
-          </label>
-          <label htmlFor="todo-category">
-            <span>Category:</span>
-            <select id="todo-category" name="category" type="text" ref={category => this.todo.category = category} placeholder="Select Category">
-              <option value="">Select Category</option>
-              {this.props.categories.map((category) => {
-                return <option value={category.id} key={category.id}>{category.name}</option>
-              })}
-            </select>
-          </label>
-          <label htmlFor="todo-datetime">
-            <span>Date:</span>
-            <input id="todo-title" name="datetime" type="datetime-local" ref={date => this.todo.date = date} placeholder="Pick Date & Time" />
-          </label>
-          <label htmlFor="todo-content">
-            <span>Content:</span>
-            <textarea id="todo-title" name="content" type="text" ref={content => this.todo.content = content} placeholder="Enter Content (optional)"></textarea>
-          </label>
-          <button onClick={this.clickHandler}>Save</button>
-        </form>
+  return (
+    <div className="form-container">
+      <div className="manage-form-buttons">
+        <Link className="button" to="/">&lt; Back</Link>
       </div>
-    );
-  }
+      <form method="" onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="todo-title">
+          <span>Title:</span>
+          <input id="todo-title" name="title" type="text" ref={register({required: "Title is required", minLength: {value: 3, message: "Title is too short"}})} placeholder="Enter ToDo title" className={errors.title ? 'input-error' : ''}/>
+          <span className="error-message">{errors.title && errors.title.message}</span>
+        </label>
+        <label htmlFor="todo-category">
+          <span>Category:</span>
+          <select id="todo-category" name="category" type="text" ref={register({required: "Category is required"})} placeholder="Select Category" className={errors.category ? 'input-error' : ''}>
+            <option value="">Select Category</option>
+            {props.categories.map((category) => {
+              return <option value={category.id} key={category.id}>{category.name}</option>
+            })}
+          </select>
+          <span className="error-message">{errors.category && errors.category.message}</span>
+        </label>
+        <label htmlFor="todo-datetime">
+          <span>Date:</span>
+          <input id="todo-title" name="datetime" type="datetime-local" ref={register({required: "Datetime is required", pattern: {value: /^\d{4}-[0-1]{1}\d{1}-[0-3]{1}\d{1} [0-2]{1}\d{1}:[0-5]{1}\d{1}$/i, message: 'Invalid datetime format'}})} placeholder="Pick Date & Time"  className={errors.datetime ? 'input-error' : ''}/>
+          <span className="error-message">{errors.datetime && errors.datetime.message}</span>
+        </label>
+        <label htmlFor="todo-content">
+          <span>Content:</span>
+          <textarea id="todo-title" name="content" type="text" ref={register} placeholder="Enter Content (optional)"></textarea>
+        </label>
+        <button>Save</button>
+      </form>
+    </div>
+  );
 }
 
 const mapStateToProps = (state) => {
